ec5ace0ab505c42d6f122596324c044c02aa851b
John Gidskehaug <gidzk@Johns-MacBook-Pro.local>
2021-12-10 02:05:35 +0100

    Setup initial page

---
+++ src/components/useMousePosition.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export const useMousePosition = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const setFromEvent = (e) => setPosition({ x: e.clientX, y: e.clientY });
+    const wrapper = document.getElementById('wrapper');
+    wrapper.addEventListener("mousemove", setFromEvent);
+
+    return () => {
+      wrapper.removeEventListener("mousemove", setFromEvent);
+    };
+  }, []);
+
+  return position;
+};
