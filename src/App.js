af255d02d581de03ff32024b8d07f958cf2fbce6
John Gidskehaug <john.gidskehaug@gmail.com>
2023-07-26 11:41:11 +0200

    Add cool fresh nice animation to cool fresh button with help from cool fresh Oskar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './style/App.scss';
-import React from 'react';
-import DiagonalBackground from './components/diagonalBackground/DiagonalBackground';
+import "./style/App.scss"
+import React from "react"
+import DiagonalBackground from "./components/diagonal-background/DiagonalBackground"
 
 function App() {
   return (
-    <section id='wrapper' className='skewed'>
-        <DiagonalBackground
-          headingMain ='John'
-          headingSecondary = 'Gidskehaug'
-          leftText = {{heading:'DEVELOPER / DESIGNER',text:'Check my work'}}
-          rightText = {{heading:'PRODUCER',text:'Check my music'}}
-        />
+    <section id="wrapper" className="skewed">
+      <DiagonalBackground
+        headingMain="John"
+        headingSecondary="Gidskehaug"
+        leftText={{ heading: "DEVELOPER ", text: "Check my work" }}
+        rightText={{ heading: "PRODUCER", text: "Check my music" }}
+      />
     </section>
-  );
+  )
 }
 
-export default App;
+export default App
